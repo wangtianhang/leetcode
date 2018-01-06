0d64a211137415ec2f795fcdab6d5bec144a1755
wangtianhang <690879430@qq.com>
2018-01-07 00:33:32 +0800

    brainteaser

---
+++ leetCode/src/319 Bulb Switcher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class _319_Bulb_Switcher
+{
+    public int BulbSwitch(int n)
+    {
+        return (int)Math.Sqrt(n);
+    }
+}
+
