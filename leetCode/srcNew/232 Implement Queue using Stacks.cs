2496dfec92ccb45ea4c7f6979bdf7c0e87f059b5
wangtianhang <690879430@qq.com>
2018-01-09 13:37:51 +0800

    stack实现queue

---
+++ leetCode/srcNew/232 Implement Queue using Stacks.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class MyQueue
+{
+    Stack<int> m_stackA = new Stack<int>();
+    Stack<int> m_stackB = new Stack<int>();
+    /** Initialize your data structure here. */
+    public MyQueue()
+    {
+
+    }
+
+    /** Push element x to the back of queue. */
+    public void Push(int x)
+    {
+        m_stackB.Push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int Pop()
+    {
+        if (Empty())
+        {
+            throw new System.Exception("can not dequeue");
+        }
+
+        if (m_stackA.Count() == 0)
+        {
+            while (m_stackB.Count() != 0)
+            {
+                m_stackA.Push(m_stackB.Pop());
+            }
+        }
+        return m_stackA.Pop();
+    }
+
+    /** Get the front element. */
+    public int Peek()
+    {
+        if (Empty())
+        {
+            throw new System.Exception("can not dequeue");
+        }
+
+        if (m_stackA.Count() == 0)
+        {
+            while (m_stackB.Count() != 0)
+            {
+                m_stackA.Push(m_stackB.Pop());
+            }
+        }
+
+        return m_stackA.Peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public bool Empty()
+    {
+        return m_stackA.Count + m_stackB.Count == 0;
+    }
+}
+
