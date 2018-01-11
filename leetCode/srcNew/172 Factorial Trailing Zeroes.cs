61186ab817e6195b312158ee9be35f21a0f0eb9d
wangtianhang <690879430@qq.com>
2018-01-11 14:49:51 +0800

    阶乘尾数0个数

---
+++ leetCode/srcNew/172 Factorial Trailing Zeroes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetCode.srcNew
+{
+    class _172_Factorial_Trailing_Zeroes
+    {
+        public static void Test()
+        {
+            _172_Factorial_Trailing_Zeroes test = new _172_Factorial_Trailing_Zeroes();
+            Console.WriteLine(test.TrailingZeroes(1808548329));
+        }
+
+        public int TrailingZeroes(int n)
+        {
+            if (n / 5 < 5)
+            {
+                return n / 5;
+            }
+            else
+            {
+                return n / 5 + TrailingZeroes(n / 5);
+            }
+        }
+    }
+}
