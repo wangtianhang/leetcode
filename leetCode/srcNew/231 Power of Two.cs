ef5e874580f9c019dbd40f6cdce1701b7434deb0
wangtianhang <690879430@qq.com>
2018-01-11 15:29:28 +0800

    is power of two

---
+++ leetCode/srcNew/231 Power of Two.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetCode.srcNew
+{
+    class _231_Power_of_Two
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return (n & (n - 1)) == 0;
+            }
+        }
+    }
+}
