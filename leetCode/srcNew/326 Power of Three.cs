bca920b489b036629503e8cd1a4b9850d2e715ae
wangtianhang <690879430@qq.com>
2018-01-11 15:42:11 +0800

    补提交

---
+++ leetCode/srcNew/326 Power of Three.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetCode.srcNew
+{
+    class _326_Power_of_Three
+    {
+        public bool IsPowerOfThree(int n)
+        {
+            if (n <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                //任何一个3的x次方一定能对int型里最大的3的x次方求模
+                if (1162261467 % n == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
