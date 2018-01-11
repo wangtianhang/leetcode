4f2cd60feee20775bbf56e0ac2c70edcfb512d4d
wangtianhang <690879430@qq.com>
2018-01-11 12:13:43 +0800

    补提交

---
+++ leetCode/srcNew/50 Pow(x, n).cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetCode.srcNew
+{
+    class _50_Pow_x__n_
+    {
+        public double MyPow(double x, int n)
+        {
+            return Math.Pow(x, n);
+        }
+    }
+}
