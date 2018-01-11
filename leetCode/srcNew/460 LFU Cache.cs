d6e8e6521dd3e81a18334474a7a1ef993dcba493
wangtianhang <690879430@qq.com>
2018-01-11 11:02:15 +0800

    发现坑bug。。dic的foreach不稳定

--- leetCode/srcNew/460 LFU Cache.cs
+++ leetCode/srcNew/460 LFU Cache.cs
@@ -175,13 +175,50 @@ public class LFUCache
         cache.Put(2, 2);
         cache.Put(4, 4);
         cache.Get(2);*/
-        LFUCache cache = new LFUCache(2);
+        /*LFUCache cache = new LFUCache(2);
         cache.Put(2, 1);
         cache.Put(1, 1);
         cache.Put(2, 3);
         cache.Put(4, 1);
         Console.WriteLine(cache.Get(1));
-        Console.WriteLine(cache.Get(2));
+        Console.WriteLine(cache.Get(2));*/
+
+        LFUCache cache = new LFUCache(10);
+        string[] op = new string[] { "put", "put", "put", "put", "put", "get", "put", "get", "get", "put", "get", "put", "put", "put", "get", "put", "get", "get", "get", "get", "put", "put", "get", "get", "get", "put", "put", "get", "put", "get", "put", "get", "get", "get", "put", "put", "put", "get", "put", "get", "get", "put", "put", "get", "put", "put", "put", "put", "get", "put", "put", "get", "put", "put", "get", "put", "put", "put", "put", "put", "get", "put", "put", "get", "put", "get", "get", "get", "put", "get", "get", "put", "put", "put", "put", "get", "put", "put", "put", "put", "get", "get", "get", "put", "put", "put", "get", "put", "put", "put", "get", "put", "put", "put", "get", "get", "get", "put", "put", "put", "put", "get", "put", "put", "put", "put", "put", "put", "put" };
+        string opParam = "[10,13],[3,17],[6,11],[10,5],[9,10],[13],[2,19],[2],[3],[5,25],[8],[9,22],[5,5],[1,30],[11],[9,12],[7],[5],[8],[9],[4,30],[9,3],[9],[10],[10],[6,14],[3,1],[3],[10,11],[8],[2,14],[1],[5],[4],[11,4],[12,24],[5,18],[13],[7,23],[8],[12],[3,27],[2,12],[5],[2,9],[13,4],[8,18],[1,7],[6],[9,29],[8,21],[5],[6,30],[1,12],[10],[4,15],[7,22],[11,26],[8,17],[9,29],[5],[3,4],[11,30],[12],[4,29],[3],[9],[6],[3,4],[1],[10],[3,29],[10,28],[1,20],[11,13],[3],[3,12],[3,8],[10,9],[3,26],[8],[7],[5],[13,17],[2,27],[11,15],[12],[9,19],[2,15],[3,16],[1],[12,17],[9,1],[6,19],[4],[5],[5],[8,1],[11,7],[5,2],[9,28],[1],[2,2],[7,4],[4,22],[7,24],[9,26],[13,28],[11,26]";
+        opParam = opParam.Replace("[", "").Replace("]", "");
+        List<int> opParamList = new List<int>();
+        string[] opParamSub = opParam.Split(',');
+        foreach(var iter in opParamSub)
+        {
+            opParamList.Add(int.Parse(iter));
+        }
+        //string[] opParamSub = opParam.Split(',');
+        for (int i = 0; i < op.Length; ++i)
+        {
+            string iter = op[i];
+            if (iter == "put")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("cache.Put " + opParamList[0] + " " + opParamList[1]);
+                Console.ForegroundColor = ConsoleColor.White;
+                cache.Put(opParamList[0], opParamList[1]);
+                DebugState(cache);
+                opParamList.RemoveAt(0);
+                opParamList.RemoveAt(0);
+                
+            }
+            else if (iter == "get")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("cache.Get " + opParamList[0]);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                cache.Get(opParamList[0]);
+                DebugState(cache);
+                opParamList.RemoveAt(0);
+            }
+        }
     }
 
     public static void DebugState(LFUCache cache)
@@ -289,10 +326,17 @@ public class LFUCache
             {
                 m_doubleLinkedList.RemoveNode(oldNode);
             }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(val);
+            Console.ForegroundColor = ConsoleColor.White;
             return val;
         }
         else
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(-1);
+            Console.ForegroundColor = ConsoleColor.White;
             return -1;
         }
     }
@@ -340,6 +384,7 @@ public class LFUCache
                 }
                 frequencyData.m_dic.Remove(firstKey);
                 m_valueDic.Remove(firstKey);
+                Console.WriteLine("remove firstKey " + firstKey + " frequency " + frequencyData.m_frequency);
                 if(frequencyData.m_dic.Count == 0)
                 {
                     m_doubleLinkedList.RemoveNode(m_doubleLinkedList.m_first);
