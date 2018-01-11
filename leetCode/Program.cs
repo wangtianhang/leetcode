ccf6ffb0e8a05a391db9d2ee83da29b3e1513188
wangtianhang <690879430@qq.com>
2018-01-11 15:53:59 +0800

    补提交

--- leetCode/Program.cs
+++ leetCode/Program.cs
@@ -14,4 +14,3 @@ class Program
         Console.ReadLine();
     }
 }
-
---
+++ leetCode/srcNew/367 Valid Perfect Square.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetCode.srcNew
+{
+    class _367_Valid_Perfect_Square
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            if (num <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                double sqrtD = Math.Sqrt(num);
+                int sqrt = (int)(sqrtD + 0.5);
+                return sqrt * sqrt == num;
+            }
+        }
+    }
+}
