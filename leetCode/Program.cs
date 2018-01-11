b75b83e009e10769d1a5304ae591c51db02741a0
wangtianhang <690879430@qq.com>
2018-01-11 12:31:53 +0800

    补提交

--- leetCode/Program.cs
+++ leetCode/Program.cs
@@ -14,3 +14,5 @@ class Program
         Console.ReadLine();
     }
 }
+
+
---
+++ leetCode/srcNew/069 Sqrt(x).cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetCode.srcNew
+{
+    class _69_Sqrt_x_
+    {
+        public int MySqrt(int x)
+        {
+            return (int)Math.Sqrt(x);
+        }
+    }
+}
