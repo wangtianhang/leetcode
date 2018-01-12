d636aaecaab562b0ac0e73ae0f96a25139bafb3c
wangtianhang <690879430@qq.com>
2018-01-12 10:57:24 +0800

    303

---
+++ leetCode/srcNew/303 Range Sum Query - Immutable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class NumArray
+{
+    int[] m_sums = null;
+    public NumArray(int[] nums)
+    {
+        if(nums.Length == 0)
+        {
+            return;
+        }
+        m_sums = new int[nums.Length];
+        m_sums[0] = nums[0];
+        for (int i = 1; i < nums.Length; ++i )
+        {
+            m_sums[i] = nums[i] + m_sums[i - 1];
+        }
+    }
+
+    public int SumRange(int i, int j)
+    {
+        int lowSum = 0;
+        if(i - 1 >= 0)
+        {
+            lowSum = m_sums[i - 1];
+        }
+        return m_sums[j] - lowSum;
+    }
+}
